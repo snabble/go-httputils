e1ec33d3a2424bc45d7be89bb5ceda6a303eb534
Jakob Rockenbauch <jakob.rockenbauch@snabble.io>
2020-08-18 11:16:34 +0200

    move request and related functions to own file

---
+++ request.go
@@ -0,0 +1,122 @@
+package httputils
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/cenkalti/backoff/v4"
+)
+
+type Encoder func(interface{}) ([]byte, error)
+
+type Decoder func([]byte, interface{}) error
+
+type Request struct {
+	RawRequest *http.Request
+	Header     http.Header
+	Encode     Encoder
+
+	Decode      Decoder
+	RawResponse *http.Response
+}
+
+func defaultRequest() *Request {
+	header := http.Header{}
+	header.Add("Content-Type", "application/json")
+
+	return &Request{
+		Header: header,
+
+		Encode: json.Marshal,
+		Decode: json.Unmarshal,
+	}
+}
+
+func (req *Request) applyHeader() {
+	for name, values := range req.Header {
+		for _, value := range values {
+			req.RawRequest.Header.Add(name, value)
+		}
+	}
+}
+
+func (req *Request) isSuccessfulPost() bool {
+	return req.RawResponse.StatusCode == http.StatusOK || req.RawResponse.StatusCode == http.StatusCreated
+}
+
+func (req Request) decodeBody(entity interface{}) error {
+	data, err := ioutil.ReadAll(req.RawResponse.Body)
+	if err != nil {
+		return fmt.Errorf("reading response body: '%w'", err)
+	}
+
+	return req.Decode(data, entity)
+}
+
+func applyParams(req *Request, params []RequestParam) {
+	for _, param := range params {
+		param(req)
+	}
+}
+
+func permanentHTTPError(resp *Request) error {
+	return backoff.Permanent(httpError(resp))
+}
+
+func httpError(resp *Request) error {
+	return HTTPClientError{Code: resp.RawResponse.StatusCode, Status: resp.RawResponse.Status}
+}
+
+type RequestParam func(*Request)
+
+func SetEncoder(e Encoder) RequestParam {
+	return func(req *Request) {
+		req.Encode = e
+	}
+}
+
+func SetDecoder(d Decoder) RequestParam {
+	return func(req *Request) {
+		req.Decode = d
+	}
+}
+
+func UserAgent(userAgent string) RequestParam {
+	return func(req *Request) {
+		req.Header.Set("User-Agent", userAgent)
+	}
+}
+
+func Accept(mediaType string) RequestParam {
+	return func(req *Request) {
+		req.Header.Add("Accept", mediaType)
+	}
+}
+
+func ClientToken(token string) RequestParam {
+	return func(req *Request) {
+		req.Header.Set("Client-Token", token)
+	}
+}
+
+func BearerAuth(token string) RequestParam {
+	return func(req *Request) {
+		req.Header.Set("Authorization", "Bearer "+token)
+	}
+}
+
+func BasicAuth(username, password string) RequestParam {
+	return func(req *Request) {
+		auth := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
+		req.Header.Set("Authorization", "Basic "+auth)
+	}
+}
+
+func ContentType(contentType string) RequestParam {
+	return func(req *Request) {
+		req.Header.Set("Content-Type", contentType)
+	}
+}
